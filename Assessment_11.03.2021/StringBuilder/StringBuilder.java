32b51b08783e4bec8be0496610c590b91cdeeca6
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-03-12 12:31:34 +0530

    Create StringBuilder.java

---
+++ Assessment_11.03.2021/StringBuilder/StringBuilder.java
@@ -0,0 +1,12 @@
+class StringBuilder{  
+
+public static void main(String args[])
+{  
+
+StringBuilder sb=new StringBuilder("Hello");  
+sb.replace(1,2,3,4,5,"Java");  
+System.out.println(sb);//prints HelloJavalo  
+
+}  
+
+}  
