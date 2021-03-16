589965fb43b550fee1899a9f53a646ba15b5a08b
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-03-16 13:09:36 +0530

    Create Main.java

---
+++ Assessment_13.03.2021/Exception Handling/NullPointer Exception/Main.java
@@ -0,0 +1,17 @@
+package ExceptionHandling;
+
+public class Main {
+
+    public static void main(String args[])
+    {
+        try{
+            String str=null;
+            System.out.println (str.length());
+        }
+        catch(NullPointerException e){
+            System.out.println("NullPointerException..");
+            e.printStackTrace();
+        }
+    }
+}
+
