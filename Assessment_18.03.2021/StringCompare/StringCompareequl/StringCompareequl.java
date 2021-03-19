87a20b419de0606b8bdee802d555e83d450c581b
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-03-19 12:42:19 +0530

    Create StringCompareequl.java

---
+++ Assessment_18.03.2021/StringCompare/StringCompareequl/StringCompareequl.java
@@ -0,0 +1,9 @@
+public class StringCompareequl{
+   public static void main(String []args){
+      String s1 = "Gopi";
+      String s2 = "Gopi";
+      String s3 = new String ("Srini");
+      System.out.println(s1 == s2);
+      System.out.println(s2 == s3);
+   }
+}
