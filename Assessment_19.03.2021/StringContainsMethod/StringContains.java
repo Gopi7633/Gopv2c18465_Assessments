d96db3e3e68b5afc0a5f895f17d55ee93e1788ae
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-03-20 12:52:53 +0530

    Create StringContains.java

---
+++ Assessment_19.03.2021/StringContainsMethod/StringContains.java
@@ -0,0 +1,28 @@
+public class StringContains {
+    public static void main(String[] args) {
+
+        String str = "Ganapathi Gopi Srini";
+
+        System.out.println("String contains 'Gopi': " + str.contains("Gopi"));
+
+        System.out.println("String contains 'Ganapathi': " + str.contains("Ganapathi"));
+
+        System.out.println("String contains 'Srini': " + str.contains("Srini"));
+
+    }
+}
+
+/*
+public class StringContains {
+    public static void main(String args[]) {
+        String str1 = "Gopi";
+
+        if (str1.contains("gopi")) {
+            System.out.println("gopi : is found in given string");
+        } else {
+            System.out.println("gopi: is not found in the string");
+        }
+    }
+}
+
+ */
