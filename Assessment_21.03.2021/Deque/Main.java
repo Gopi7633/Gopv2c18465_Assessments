f3c1e85d647a743b2b6066c263756e6e8264950f
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-03-23 13:44:09 +0530

    Update Main.java

--- Assessment_21.03.2021/Deque/Main.java
+++ Assessment_21.03.2021/Deque/Main.java
@@ -1,1 +1,28 @@
-...
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class ArrayDeque{
+
+    public static void main(String[] args) {
+
+        Deque<String> dq = new ArrayDeque<String>();
+
+        dq.add("Gopi");
+        dq.add("Ganapathi");
+        dq.addLast("Srini");
+        dq.addFirst("Shankar");
+        dq.add("prathap");
+
+        System.out.println("Elements in Deque:"+dq);
+
+        System.out.println("Removed element: "+dq.removeLast());
+
+        System.out.println("Head: "+dq.element());
+
+        System.out.println("poll(): "+dq.pollLast());
+
+        System.out.println("peek(): "+dq.peek());
+        
+        System.out.println("Elements in Deque:"+dq);
+    }
+}
