3eae5fed5da1f990e41fda033f1ce48710197a85
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-04-02 15:57:16 +0530

    Update Main.java

--- Assessment_1.04.2021/StringLength/Main.java
+++ Assessment_1.04.2021/StringLength/Main.java
@@ -1,1 +1,18 @@
-...
+package lambdaExpression.com;
+
+public class StringLength {
+    public static void main(String args[])
+    {
+        Length a = (s)->
+        {
+            int len=s.length();
+            System.out.println("Length of the String is: " + s.length());
+        };
+        a.length("GopiT");
+    }
+}
+
+interface Length
+{
+   public void length(String s);
+}
