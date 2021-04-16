15fdf9b11445684f6b328415b502d52b9dd04255
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-04-16 09:39:54 +0530

    Update Main.java

--- Assessment_13.04.2021/LocalDateTime/Main.java
+++ Assessment_13.04.2021/LocalDateTime/Main.java
@@ -1,1 +1,24 @@
+package com.jodadatetime;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+	public static void main(String[] args) {
+		LocalDate d1 = LocalDate.now();
+		System.out.println("Show Today Date : " + d1);
+		
+		LocalDate d2 = LocalDate.of(1997, 05, 11);
+		System.out.println("One Year Later : " + d2.plusMonths(12));
+		
+		LocalDate d3 = LocalDate.parse("1990-12-20",DateTimeFormatter.ISO_DATE);
+		System.out.println(d3);
+		
+		LocalDateTime d4 = LocalDateTime.now();
+		System.out.println("Show Today Date Time : " + d4);
+		
+		LocalDateTime d5 = LocalDateTime.of(1995, 05, 20, 15, 45);
+		System.out.println("Add year : " + d5.plusYears(5));
+	}
+}
