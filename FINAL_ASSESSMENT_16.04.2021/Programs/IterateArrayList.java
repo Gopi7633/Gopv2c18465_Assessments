6b412cdefd96667115917447b2cafbe40ee8187d
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-04-16 19:50:30 +0530

    Create IterateArrayList.java

---
+++ FINAL_ASSESSMENT_16.04.2021/Programs/IterateArrayList.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IterateArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> list = new ArrayList<String>(); 
+		
+		 list.add("Jan");  
+		 list.add("Feb");    
+		 list.add("Mar");    
+		 list.add("Apr"); 
+		 list.add("May");
+		 list.add("june");
+		 list.add("July");
+		 list.add("Aug");
+		 list.add("Sep");
+		 
+		 Iterator<String> itr = list.iterator();
+		 while (itr.hasNext()) {
+			System.out.println(itr.next());
+			}
+	}
+}
