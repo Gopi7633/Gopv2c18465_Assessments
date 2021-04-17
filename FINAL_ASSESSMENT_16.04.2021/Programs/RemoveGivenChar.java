8c030c046334ef799c66ee9ee82d466f6d6c44ec
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-04-17 09:54:37 +0530

    Update RemoveGivenChar.java

--- FINAL_ASSESSMENT_16.04.2021/Programs/RemoveGivenChar.java
+++ FINAL_ASSESSMENT_16.04.2021/Programs/RemoveGivenChar.java
@@ -1,13 +1,31 @@
 package com.removegivenchar;
 
 public class RemoveGivenChar {
+	
+	static void removeChar(String s, char c)
+	{
+		int j, count = 0, n = s.length();
+		char []t = s.toCharArray();
+		for (int i = j = 0; i < n; i++)
+		{
+			if (t[i] != c)
+			t[j++] = t[i];
+			else
+				count++;
+		}
+		
+		while(count > 0)
+		{
+			t[j++] = '\0';
+			count--;
+		}
+		
+		System.out.println(t);
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str = "GopiT";  
-        System.out.println(charRemoveAt(str, 4));  
-     }  
-     static String charRemoveAt(String str, int p) {  
-        return str.substring(0, p) + str.substring(p + 1);  
-     }  
+	public static void main(String[] args)
+	{
+		String s = "Programming";
+		removeChar(s, 'm');
+	}
 }
