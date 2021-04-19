3f5df3cfc5069e16cf4ca4025824a87f7060c37d
Gopi <72385378+Gopi7633@users.noreply.github.com>
2021-04-19 10:51:12 +0530

    Update ReadFileWriteIntoAnotherFile.java

--- FINAL_ASSESSMENT_17.04.2021/ReadFileWriteIntoAnotherFile.java
+++ FINAL_ASSESSMENT_17.04.2021/ReadFileWriteIntoAnotherFile.java
@@ -1,32 +1,40 @@
 package com.readonewriteanotherfile;
 
-import java.io.File;
-import java.io.IOException;
+package com.readonefilewriteanother;
 
-public class ReadFileWriteIntoAnotherFile {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-		      File myObj = new File("D:\\Eclipse-New Work space\\Final Assessment1\\src\\com\\readonewriteanotherfile\\filename.txt");
-		      if (myObj.createNewFile()) {
-		        System.out.println("File created: " + myObj.getName());
-		      } else {
-		        System.out.println("File already exists.");
-		      }
+public class Main {
 
-		      File myObj1 = new File("D:\\Eclipse-New Work space\\Final Assessment1\\src\\com\\readonewriteanotherfile\\namefile.txt");
-		      if (myObj1.createNewFile()) {
-					System.out.println("File created: " + myObj1.getName());
-				}
-		      else {
-		    	  System.out.println("Error");
-		      }
-		      }
-		catch (IOException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-	}
+   public static void main(String[] args) {
 
-}
+     try {
+
+           FileInputStream fin = new FileInputStream("C:\\Users\\gopv2c18465\\git\\FinalAssessment2\\Final Assessment1\\filename.txt");
+
+           int i = 0;
+           String s = "";
+
+           while((i=fin.read())!=-1) {
+
+               s = s + String.valueOf((char)i);
+
+           }
+
+           FileOutputStream fout = new 
+           FileOutputStream("D:\\newtest1.txt");
+           byte[] b = s.getBytes();
+
+           fout.write(b);
+           fout.close();
+
+           System.out.println("reading and writing are completed !!");
+
+      } catch(Exception e){
+         System.out.println(e);
+      }
+
+    }
+
+ }
